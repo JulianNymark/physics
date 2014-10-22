06a075334fd48eb3f95f4f34f5df9d0ff7599c5f
Julian Hisdal Nymark <julianymark@gmail.com>
2014-10-23 01:21:08 +0200

    almost done

--- collision/output/source/collision.java
+++ collision/output/source/collision.java
@@ -18,11 +18,13 @@ class Ball {
     public PVector location;
     public PVector velocity;
     public PVector acceleration;
+    public PVector radius;
 
     Ball( PVector l, PVector v, PVector a ){
 	location = l;
 	velocity = v;
 	acceleration = a;
+	radius = new PVector(20, 20);
     }
 }
 
@@ -30,12 +32,16 @@ ArrayList<Ball> al;
 PVector mouseOld;
 PVector g = new PVector(0, 900); // pixels
 
+float DAMPEN = 0.90f;
+
 public void setup(){
     size(1600,900);
 
     al = new ArrayList<Ball>();
 
     mouseOld = new PVector();
+
+    ellipseMode(RADIUS);
 }
 
 public void draw() {
@@ -66,19 +72,59 @@ public void physics( float dt ) {
 
     // boundary check
     for (Ball b : al) {
-	if (b.location.x > displayWidth || b.location.x < 0 ) {
+	if (b.location.x > width - b.radius.x ) {
 	    b.velocity.x *= -1;
-	    b.velocity.mult(0.99f);
+	    b.velocity.mult(DAMPEN);
+	    b.location.x = width - b.radius.x;
 	}
-	if (b.location.y > displayHeight || b.location.y < 0 ) {
+	if (b.location.x < 0 + b.radius.x) {
+	    b.velocity.x *= -1;
+	    b.velocity.mult(DAMPEN);
+	    b.location.x = 0 + b.radius.x;
+	}
+	if (b.location.y > height - b.radius.y) {
 	    b.velocity.y *= -1;
-	    b.velocity.mult(0.99f);
+	    b.velocity.mult(DAMPEN);
+	    b.location.y = height - b.radius.y;
+	}
+	if (b.location.y < 0 + b.radius.y) {
+	    b.velocity.y *= -1;
+	    b.velocity.mult(DAMPEN);
+	    b.location.y = 0 + b.radius.y;
 	}
     }
     
     // circle to circle
-    
-    
+    System.out.println(al.size());
+    for (int i = 0; i < al.size(); ++i) {
+	for (int i2 = i; i < al.size(); ++i) {
+	    Ball b = al.get(i);
+	    Ball b2 = al.get(i2);
+	    
+	    if (!(i == i2)) {
+		// detect collision
+		if (b.location.dist(b2.location) < b.radius.x + b2.radius.x) {
+		    // resolve collision
+		    PVector vec_delta = b.location.get();
+		    vec_delta.sub(b2.location);
+		    float angle_delta = vec_delta.heading();
+		    
+		    // rotate vel
+		    b2.velocity.rotate(-angle_delta);
+		    b.velocity.rotate(-angle_delta);
+		    
+		    // swap x values of velocities
+		    float temp = b.velocity.x;
+		    b.velocity.x = b2.velocity.x;
+		    b2.velocity.x = temp;
+		    
+		    // rotate back
+		    b.velocity.rotate(angle_delta);
+		    b2.velocity.rotate(angle_delta);
+		}
+	    }
+	}
+    }
 }
 
 public void draw_stuff() {
@@ -86,7 +132,7 @@ public void draw_stuff() {
     fill(255, 0, 0);
     stroke(0);
     for (Ball b : al) {
-	ellipse(b.location.x, b.location.y, 20, 20);
+	ellipse(b.location.x, b.location.y, b.radius.x, b.radius.y);
     }
     
     // draw mouse-drag
